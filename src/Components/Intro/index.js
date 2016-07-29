e4e7d184965368bf715e742225bb9f62b5ebcfb7
lazaruslarue <lazaruslarue@gmail.com>
2016-07-28 17:57:12 -0700

    button css

--- src/Components/Intro/index.js
+++ src/Components/Intro/index.js
@@ -1,18 +1,19 @@
 import xs from 'xstream'
-import {div, h1, h2, span, ul, li, a, button, img, iframe} from '@cycle/dom'
-// require('./style.scss')
+import {div, h1, h2, span, ul, li, a, p, img} from '@cycle/dom'
+require('./style.scss')
 
+// <p><a href = '#' class="btn anim">Jiggle</a></p>
 
 export default function Intro(sources){
   const sinks = {
     DOM: xs.of(
       div('.intro', [
         div('.mask', [
-          div('.axis-logo', 'test'),
+          div('.axis-logo'),
           div('.mask-text', 'an american made precision smoking heirloom')
         ]),
-        div('.buy-button', [
-          button('.buy-now', 'order now')
+        p( [
+          a('.btn.anim', 'order now')
         ])
       ])
     )
