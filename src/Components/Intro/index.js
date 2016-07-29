4ac9ee67794c455a18bbc0cf50cdcec5f3e8cb89
lazaruslarue <lazaruslarue@gmail.com>
2016-07-28 20:46:56 -0700

    single image header

--- src/Components/Intro/index.js
+++ src/Components/Intro/index.js
@@ -1,20 +1,12 @@
 import xs from 'xstream'
-import {div, h1, h2, span, ul, li, a, p, img} from '@cycle/dom'
+import {div, a} from '@cycle/dom'
 require('./style.scss')
-
-// <p><a href = '#' class="btn anim">Jiggle</a></p>
 
 export default function Intro(sources){
   const sinks = {
     DOM: xs.of(
-      div('.intro', [
-        div('.mask', [
-          div('.axis-logo'),
-          div('.mask-text', 'an american made precision smoking heirloom')
-        ]),
-        p( [
-          a('.btn.anim', 'order now')
-        ])
+      div('.intro.flexcontainer.column', [
+        a('.btn.anim', 'order now')
       ])
     )
   }
