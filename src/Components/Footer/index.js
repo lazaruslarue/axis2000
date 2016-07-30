bfaa1fee6cc300b292bc55e574a811ab87318384
lazaruslarue <lazaruslarue@gmail.com>
2016-07-30 10:17:34 -0700

    first draft complete

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,16 +1,34 @@
 import xs from 'xstream'
-import {div, a} from '@cycle/dom'
+import {div, a, form, input} from '@cycle/dom'
 require('./style.scss')
 
 export default function Footer(sources){
   const sinks = {
     DOM: xs.of(
       div('#footer.flexcontainer',[
-        div('.buy-button', [
-          a( '.btn.anim', 'order now')
-        ]),
+        form('#paypal', { props: {
+          action: 'https://www.paypal.com/cgi-bin/webscr',
+          method: 'post',
+          target: '_top'
+        }}, [
+          input({props: {
+            type: 'hidden',
+            name: 'cmd',
+            value: '_s-xclick'
+          }}),
+          input({props: {
+            type: 'hidden',
+            name: 'hosted_button_id',
+            value: 'M8YBF5EK6H9QL'
+          }}),
+          input('.btn.anim',{props: {
+            type: 'submit',
+            name: 'submit',
+            value: 'order now'
+          }}),
       ])
-    )
+    ])
+  )
   }
   return sinks
 }
--- src/Components/Intro/index.js
+++ src/Components/Intro/index.js
@@ -1,12 +1,35 @@
 import xs from 'xstream'
-import {div, a} from '@cycle/dom'
+import {div, a, form, input} from '@cycle/dom'
 require('./style.scss')
 
 export default function Intro(sources){
   const sinks = {
     DOM: xs.of(
-      div('.intro.flexcontainer.column-reverse', [
-        a('.btn.anim', { props: {href: '/buy-now'} },'order now')
+
+        div('.intro.flexcontainer.column-reverse', [
+          form('#paypal', { props: {
+            action: 'https://www.paypal.com/cgi-bin/webscr',
+            method: 'post',
+            target: '_top'
+          }}, [
+            input({props: {
+              type: 'hidden',
+              name: 'cmd',
+              value: '_s-xclick'
+            }}),
+            input({props: {
+              type: 'hidden',
+              name: 'hosted_button_id',
+              value: 'M8YBF5EK6H9QL'
+            }}),
+            input('.btn.anim',{props: {
+              type: 'submit',
+              name: 'submit',
+              value: 'order now'
+            }}),
+          // a('.btn.anim', { props: {href: '/buy-now'} },'order now')
+        ])
+
       ])
     )
   }
