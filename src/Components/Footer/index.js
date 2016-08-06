44d67d0ac50f850a935334bca3f6878beb7e63d8
lazaruslarue <lazaruslarue@gmail.com>
2016-08-06 07:21:41 -0700

    update button font sizes, add Google Analytics

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,10 +1,11 @@
 import xs from 'xstream'
-import {div, a, form, input} from '@cycle/dom'
+import {div, a, form, input, script} from '@cycle/dom'
 require('./style.scss')
 
 export default function Footer(sources){
   const sinks = {
     DOM: xs.of(
+      script("          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');          ga('create', 'UA-62092769-1', 'auto');          ga('send', 'pageview');"),
       div('#footer.flexcontainer',[
         form('#paypal', { props: {
           action: 'https://www.paypal.com/cgi-bin/webscr',
