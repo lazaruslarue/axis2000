8035854819287278938d2b597a8e7c5d51fe17b0
lazaruslarue <lazaruslarue@gmail.com>
2016-08-23 17:42:49 -0600

    update text. add comments.

--- src/Components/Navigation/index.js
+++ src/Components/Navigation/index.js
@@ -4,11 +4,14 @@ import {ul, li, a} from '@cycle/dom'
 require('./style.scss')
 
 export default function Navigation(sources){
+
+  // todo: catch events & don't let the <a> redirection happen
+  
   const sinks = {
     DOM: xs.of(
       ul('.navigation', [
         li('.howto',[
-          a('.link', { props: {href: '/how-to'} }, 'helpful how-to')
+          a('.link', { props: {href: '/hints'} }, 'helpful hints')
         ]),
         li('.instructions',[
           a('.link', { props: {href: '/instructions'} }, 'instructions')
