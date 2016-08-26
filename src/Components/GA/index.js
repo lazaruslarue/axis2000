3c86137097b5898bab4e8dcdf828ae12f2518a05
lazaruslarue <lazaruslarue@gmail.com>
2016-08-26 00:56:32 -0600

    google analytics

---
+++ src/Components/GA/index.js
@@ -0,0 +1,12 @@
+import xs from 'xstream'
+import {script} from '@cycle/dom'
+
+export default function GA(sources){
+
+  const sinks = {
+    DOM: xs.of(
+      "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');          ga('create', 'UA-62092769-1', 'auto');          ga('send', 'pageview');")
+
+  }
+  return sinks
+}
