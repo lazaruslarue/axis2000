dd48037e3583cfa4b8d5b70371f3ae8641eacac3
lazaruslarue <lazaruslarue@gmail.com>
2016-08-26 00:40:55 -0600

    use button

--- src/Components/Intro/index.js
+++ src/Components/Intro/index.js
@@ -1,37 +1,17 @@
 import xs from 'xstream'
 import {div, a, form, input} from '@cycle/dom'
+import BuyButton from '../BuyButton'
 require('./style.scss')
 
 export default function Intro(sources){
-  const sinks = {
-    DOM: xs.of(
+  let button$ = BuyButton().DOM;
 
-        div('.intro.flexcontainer.column-reverse', [
-          form('#paypal', { props: {
-            action: 'https://www.paypal.com/cgi-bin/webscr',
-            method: 'post',
-            target: '_top'
-          }}, [
-            input({props: {
-              type: 'hidden',
-              name: 'cmd',
-              value: '_s-xclick'
-            }}),
-            input({props: {
-              type: 'hidden',
-              name: 'hosted_button_id',
-              value: 'M8YBF5EK6H9QL'
-            }}),
-            input('.btn.anim',{props: {
-              type: 'submit',
-              name: 'submit',
-              value: 'order now'
-            }}),
-          // a('.btn.anim', { props: {href: '/buy-now'} },'order now')
-        ])
-
+  return {
+    DOM: button$
+    .map(button => {
+      return div('.intro.flexcontainer.column-reverse', [
+        button
       ])
-    )
+    })
   }
-  return sinks
 }
