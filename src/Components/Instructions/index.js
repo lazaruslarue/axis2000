258552c9d9eaef84ede1a86f361e872dd5786de1
lazaruslarue <lazaruslarue@gmail.com>
2016-08-26 00:15:34 -0600

    convert to new data format

--- src/Components/Instructions/index.js
+++ src/Components/Instructions/index.js
@@ -1,12 +1,21 @@
 import xs from 'xstream'
-import {div, span, p, h1, img, ul, li} from '@cycle/dom'
-import {instructions} from './new_data.js'
+import {div, span, p, h1, h2, img, ul, li} from '@cycle/dom'
+import {instructions} from './data'
+import LogoHeader from '../LogoHeader'
+
 require('./style.scss')
 
 export default function Instructions(sources) {
-  let header$ = xs.of( div('.instructions-header', [
-    h1('Instructions for Use')
-  ]))
+  let logo$ = LogoHeader().DOM;
+
+  let text_header$ = xs.of(h1('Instructions for Use'))
+
+  let header$ = xs.combine(logo$, text_header$)
+    .map(([logo, text]) => {
+      return div('.page-header.flexcontainer.column', [
+        logo, text
+      ])
+    })
 
   // TODO: this should be a reusable component
   let instructions$ = xs.fromArray(instructions)
@@ -15,15 +24,20 @@ export default function Instructions(sources) {
 
       data.group_items.forEach((instruction) => {
         var ele = div('.instruction'+ instruction.orientation, [
-          instruction.text,
-          instruction.img,
+          p(instruction.text),
+          instruction.img ? img('.photo', {
+            props: {
+              src: instruction.img,
+            }
+          }) : null,
         ])
+
         return instructions.push(ele)
       })
 
       let instruction = span('.instruction-group', [
         div('.flexcontainer.column.wrapper', [
-          data.group_heading,
+          h2('.group_heading', data.group_heading),
           div('.instruction-steps', instructions)
         ])
       ])
@@ -32,7 +46,6 @@ export default function Instructions(sources) {
     })
     .fold((acc,x)=> {
       acc.push(x);
-      console.log(acc);
       return acc;
     }, [])
     .map(list => div('.instructions-body', list))
@@ -43,9 +56,9 @@ export default function Instructions(sources) {
     ])
   ]))
 
-  let vDom$ = xs.combine( header$, instructions$, footer$)
+  let vDom$ = xs.combine(  header$, instructions$, footer$)
     .map(([head, content, foot]) => {
-      return div('.instructions.flexcontainer.column', [head, content, foot])
+      return div('.instructions.flexcontainer.column', [ head, content, foot])
     })
 
   return {
