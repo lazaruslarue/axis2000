d02feaf3e34dedf92ae0d45aba115c75255347e9
lazaruslarue <lazaruslarue@gmail.com>
2016-08-29 14:03:10 -0600

    reconfigure configuration of the components

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import {div, span, p, h1, h2, img, ul, li} from '@cycle/dom'
 // Main.js in Application
 import xs from 'xstream'
 import {run} from '@cycle/xstream-run'
@@ -8,29 +9,74 @@ import {createHistory} from 'history'
 import Axis from './Components/Axis'
 import Hints from './Components/Hints'
 import Instructions from './Components/Instructions'
+// Static Components
+import GA from './Components/GA'
+import Intro from './Components/Intro'
+import Testimonials from './Components/Testimonials'
+import VideoInstructions from './Components/VideoInstructions'
+import Share from './Components/Share'
+import Navigation from './Components/Navigation'
+import Footer from './Components/Footer'
+
 
 function main(sources) {
-  const match$ = sources.router.define({
-    '/': Axis, // Main page
-    '/hints': Hints, // Hints page
-    '/instructions': Instructions, // Instructions page
-  });
 
-  const page$ = match$.map(({path, value}) => {
-    return value(Object.assign({}, sources, {
-      router: sources.router.path(path)
-    }))
-  });
+  // application state stream
+  const state$ = model(sources)
+
+  // application view combines static components
+  const view$ = view(state$, sources)
 
   return {
-    DOM: page$.map(c => c.DOM).flatten()
+    DOM: view$
   }
 }
 
 const drivers = {
   DOM: makeDOMDriver('#root'),
-  HTTP: makeHTTPDriver(),
+  // HTTP: makeHTTPDriver(),
   router: makeRouterDriver(createHistory()),
 };
 
 run(main, drivers);
+
+
+function model(sources) {
+  const routedContent$ = sources.router.define({
+    '/': Axis, // Main page
+    '/hints': Hints, // Hints page
+    '/instructions': Instructions, // Instructions page
+  });
+
+  // page content is the content from the router
+  const pageContent$ = routedContent$.map(({path, value}) => {
+     return value(Object.assign({}, sources, {
+       router: sources.router.path(path)
+     }))
+   });
+
+  return pageContent$
+}
+
+function view(state$, sources) {
+  let intro$ = Intro().DOM;
+  let navigation$ = Navigation(sources).DOM;
+  let footer$ = Footer().DOM;
+
+  let header$ = xs.combine(intro$, navigation$)
+    .map(([intro, navigation]) => {
+      return div('.page-header.flexcontainer.column', [
+        intro, navigation
+      ])
+    })
+
+  const contentDOM$ = state$.map(c => c.DOM).flatten()
+  let wholeVtree$ = xs.combine( header$, navigation$, contentDOM$, footer$)
+    .map(([head, nav, content, foot]) => {
+      return div('.flexcontainer.column', [
+        head, content, foot
+      ])
+    })
+
+  return wholeVtree$
+}
